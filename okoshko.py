d8f345e3733347bbd280c172d591f76bf9cdfaea
Ram <n1ghtdr3am3r@ya.ru>
2024-01-22 23:43:45 +0500

    добавлен размер

--- okoshko.py
+++ okoshko.py
@@ -1,7 +1,9 @@
 import tkinter as tk
 window = tk.Tk()
 greeting = tk.Label(text = 'Hello, world!',
-                   foreground = 'blue')
+                   foreground = 'blue',
+                    width = 20,
+                    height = 20)
 greeting.pack()
 
 window.mainloop()
