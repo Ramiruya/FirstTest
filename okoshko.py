2a3369505cff475f72d2b73f3ae1c4016494a017
Ram <n1ghtdr3am3r@ya.ru>
2024-01-24 09:53:40 +0500

    добавлена кнопка

--- okoshko.py
+++ okoshko.py
@@ -4,6 +4,14 @@ greeting = tk.Label(text = 'Hello, world!',
                    foreground = 'blue',
                     width = 20,
                     height = 20)
+button = tk.Button(
+    text = 'покакать',
+    width = 25,
+    height = 5,
+    bg = 'green',
+    fg = 'red',
+    )
+button.pack()
 greeting.pack()
 
 window.mainloop()
